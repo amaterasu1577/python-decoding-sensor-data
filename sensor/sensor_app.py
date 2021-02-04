7794ccad9108aec6aa56687dfa0511e442dc2397
amaterasu1577 <stfm77@gmail.com>
2021-02-04 22:28:49 +0100

    Final fix I hope

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -6,7 +6,6 @@ from load_data import load_sensor_data
 # Do not remove these two lines
 # They are needed to validate your unittest
 data = []
-
 print("Sensor Data App")
 ##############################
 
