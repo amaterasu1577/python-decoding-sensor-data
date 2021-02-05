c8876df55e89552f6b9df7af1d417ca0af463b19
amaterasu1577 <stfm77@gmail.com>
2021-02-05 22:46:10 +0100

    Section 3 completed

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,7 +1,7 @@
 # Runner script for all modules
 from load_data import load_sensor_data
 from house_info import HouseInfo
-from datetime import datetime
+from datetime import date, datetime
 
 
 ##############################
